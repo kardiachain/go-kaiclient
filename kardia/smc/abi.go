a4a58f6ffb4ee745f314d823aaa2f85ff7eedea3
Long Nguyen <ngdlong91@gmail.com>
2021-07-01 13:25:46 +0700

    Update KRC20 ABI
    Signed-off-by: Long Nguyen <ngdlong91@gmail.com>

--- kardia/smc/abi.go
+++ kardia/smc/abi.go
@@ -2519,6 +2519,51 @@ const (
 	{
 		"constant": true,
 		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
 		"name": "totalSupply",
 		"outputs": [
 			{
